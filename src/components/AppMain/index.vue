d79e33c47498cb75dea6b6292ff82a263a8c167c
孙傲 <2534658059@qq.com>
2020-09-06 23:24:17 +0800

    add link component

--- src/components/AppMain/index.vue
+++ src/components/AppMain/index.vue
@@ -1,22 +1,16 @@
 <template>
     <div class = "main">
-        <!--首页不显示面包屑-->
-        <el-breadcrumb  v-show="$route.path !== '/home'" separator="/">
-            <el-breadcrumb-item class='line' :to="{ path: $route.path }">{{$route.meta.title}}</el-breadcrumb-item> 
-        </el-breadcrumb>
+        <!--use link -->
+        <app-link></app-link>
         <router-view></router-view>
     </div>
 </template>
 
-<style scoped>
-    .el-breadcrumb{
-        height: 10px;
-        padding: 20px;
-        border-radius: 4px;
-        box-shadow: 0 2px 12px 0 rgba(0,0,0,0.1);
+<script>
+    //引入该文件下的link组件
+    import AppLink from './link.vue'
+    export default {
+        components:{AppLink}
     }
-    .line{
-        border-left:3px solid #31c17b;
-        padding-left: 10px;
-    }
-</style>
+
+</script>
